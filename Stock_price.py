6402a77f75e204d7409f7bb79661edd739cdcf86
bettinavass <vassbetka@gmail.com>
2018-09-23 14:54:47 +0000

    made the first function

--- Stock_price.py
+++ Stock_price.py
@@ -1,26 +1,33 @@
-while True:
-    shares = input("Enter number of shares: ")
-    try:
-        shares = int(shares)
-        break
-    except:
-        print("Invalid number!")
-
-
-while True:
-    price = input("Enter price (dollars, numerator, denominator): ")
-    try:
-        num = price.split()
-
-        num1 = int(num[0])
-        num2 = int(num[1])
-        num3 = int(num[2])
-        value = num1 + (num2/num3)
-        break
-    except:
-        print("Invalid price!")
+def get_shares():
+    while True:
+        sharess = input("Enter number of shares: ")
+        try:
+            sharess = int(sharess)
+            break
+        except:
+            print("Invalid number!")
+    return sharess
 
 
 
+answer = "y"
 
-print(shares, "shares with market price",num1, num2, num3, "have value", "$", value )
+while answer in ['y', 'Y']:
+    shares = get_shares()
+
+
+    while True:
+        price = input("Enter price (dollars, numerator, denominator): ")
+        try:
+            num = price.split()
+            num1 = int(num[0])
+            num2 = int(num[1])
+            num3 = int(num[2])
+            break
+        except:
+            print("Invalid price!")
+
+    value = num1 + (num2/num3)
+    print("{0} shares with market price {1} {2}/{3} have value ${4:.2f}".format(shares, num1, num2, num3, value * shares))
+
+    answer = input("Continue: ")
