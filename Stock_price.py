c83c643909ac0967bd0574a99fd56c9b6a6c11f3
bettinavass <vassbetka@gmail.com>
2018-09-23 12:56:32 +0000

    wrote the first part of the program

--- Stock_price.py
+++ Stock_price.py
@@ -1,1 +1,14 @@
-# To be program
+shares = input("Enter number of shares: ")
+
+price = input("Enter price (dollar, numerator,denominator): ")
+
+num = price.split()
+
+num1 = int(num[0])
+num2 = int(num[1])
+num3 = int(num[2])
+
+value = num1 + (num2/num3)
+
+
+print(shares, "shares with market price",num1, num2, num3, "have value", "$", value )
